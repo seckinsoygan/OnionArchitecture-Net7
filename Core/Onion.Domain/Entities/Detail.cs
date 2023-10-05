7eb8ecbad5804561088bf19adea5feacf829aa90
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-10-06 01:47:20 +0300

    UnitofWork added

--- Core/Onion.Domain/Entities/Detail.cs
+++ Core/Onion.Domain/Entities/Detail.cs
@@ -14,9 +14,9 @@ namespace Onion.Domain.Entities
             this.Description = Description;
             this.CategoryId = CategoryId;
         }
-        public required string Title { get; set; }
-        public required string Description { get; set; }
-        public required int CategoryId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
         public string Category { get; set; }
     }
 }
