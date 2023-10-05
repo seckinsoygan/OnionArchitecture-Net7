91c470452765319a56d78b50c1bbe6bd53327011
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-10-06 02:15:57 +0300

    CQRS and MediaTr added

---
+++ Core/Onion.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Onion.Application.Interfaces.UnitOfWorks;
+using Onion.Domain.Entities;
+
+namespace Onion.Application.Features.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQueryRequest, IList<GetAllProductsQueryResponse>>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public GetAllProductsQueryHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public async Task<IList<GetAllProductsQueryResponse>> Handle(GetAllProductsQueryRequest request, CancellationToken cancellationToken)
+        {
+            var products = await unitOfWork.GetReadRepository<Product>().GetAllAsync();
+
+            List<GetAllProductsQueryResponse> response = new();
+
+            foreach (var product in products)
+                response.Add(new GetAllProductsQueryResponse
+                {
+                    Title = product.Title,
+                    Description = product.Description,
+                    Discount = product.Discount,
+                    Price = product.Price - (product.Price * product.Discount / 100),
+                });
+
+            return response;
+        }
+    }
+}
